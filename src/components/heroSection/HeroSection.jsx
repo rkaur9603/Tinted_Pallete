857c2dcf1779fea193f005a6e2115f763cf551cd
Rashmeet Kaur <122515514+rkaur9603@users.noreply.github.com>
2024-08-29 16:02:36 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,10 +1,9 @@
-const HeroSection= ()=> {
+const HeroSection = () => {
   return (
-    <div>
-      <img className="h-40 lg:h-full" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/63915d104315617.5f6084db8b877.jpg"  alt=""/>
+    <div className="w-full overflow-hidden">
+      <img className="w-full h-40 lg:h-full object-cover" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/63915d104315617.5f6084db8b877.jpg" alt="Hero" />
     </div>
   );
 }
 
-export default HeroSection
-
+export default HeroSection;
